b10dfbe3e730b8bf933e530f290b50bb7e9185e5
Ilyyaa <48884134+Ilyyaa@users.noreply.github.com>
2023-06-14 15:38:55 +0300

    Update Laba7.cpp

--- Laba7/Laba7.cpp
+++ Laba7/Laba7.cpp
@@ -79,3 +79,68 @@ int partition(int arr[], int low, int high) {
     swap(&arr[i], &arr[high]);
     return i;
 }
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// Функция для обмена элементов
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+// Функция разбиения массива и возврата индекса опорного элемента
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];  // Выбираем опорный элемент
+    int i = low;  // Индекс меньшего элемента
+
+    for (int j = low; j <= high - 1; j++) {
+        // Если текущий элемент меньше или равен опорному
+        if (arr[j] <= pivot) {
+            swap(&arr[i], &arr[j]);
+            i++;  // Увеличиваем индекс меньшего элемента
+        }
+    }
+    swap(&arr[i], &arr[high]);
+    return i;
+}
+
+// Функция поиска порядковой статистики с использованием алгоритма QuickSelect
+int quickSelect(int arr[], int low, int high, int k) {
+    if (low == high) {  // Базовый случай - массив из одного элемента
+        return arr[low];
+    }
+
+    // Разбиение массива
+    int pivotIndex = partition(arr, low, high);
+
+    if (k == pivotIndex) {  // Порядковая статистика найдена
+        return arr[pivotIndex];
+    } else if (k < pivotIndex) {  // Искомый элемент находится в левой части
+        return quickSelect(arr, low, pivotIndex - 1, k);
+    } else {  // Искомый элемент находится в правой части
+        return quickSelect(arr, pivotIndex + 1, high, k);
+    }
+}
+
+// Функция поиска порядковой статистики
+int findOrderStatistic(int arr[], int n, int k) {
+    if (k > 0 && k <= n) {
+        return quickSelect(arr, 0, n - 1, k - 1);
+    }
+    return -1;  // Некорректное значение k
+}
+
+// Тестирование функции
+int main() {
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 3;
+
+    cout << "Исходный массив: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl
